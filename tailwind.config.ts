c5cd7d51df184c5c056338e5274722ab5c5aa4b0
OGTutor <i.send.you.a.letter@gmail.com>
2024-03-01 21:21:13 +0200

    Added custom classes for screen extensions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,13 @@ const config: Config = {
 		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
-		extend: {
-			backgroundImage: {
-				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-				'gradient-conic':
-					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-			},
+		extend: {},
+		screens: {
+			xl: { max: '1200px' },
+			lg: { max: '991px' },
+			md: { max: '767px' },
+			sm: { max: '550px' },
+			xsm: { max: '375px' },
 		},
 	},
 	plugins: [],
