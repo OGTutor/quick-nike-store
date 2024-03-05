f2314e1ede05b2b3acfb58c4d213d21538874f90
OGTutor <i.send.you.a.letter@gmail.com>
2024-03-05 21:00:06 +0200

    Implemented a basic Title component

---
+++ src/components/Home/ui/Title.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+
+interface TitleProps {
+	title: string;
+}
+
+const Title: FC<TitleProps> = ({ title }) => {
+	return (
+		<div className="grid items-center">
+			<h1 className="text-5xl lg:text-4xl md:text-3xl font-bold text-slate-900 filter drop-shadow-lg">
+				{title}
+			</h1>
+		</div>
+	);
+};
+
+export default Title;
